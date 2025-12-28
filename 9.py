0d3b1f74fa5c604b7eb51226716c3f6a77c4a354
Harshini Senthilkumar <harshinisenthil@myyahoo.com>
2025-12-28 23:43:12 +0530

    Delete 9.py

--- 9.py
+++
@@ -1,7 +0,0 @@
-print("Enter two of your exam scores.")
-score1=int(input("Score 1:"))
-score2=int(input("Score 2:"))
-if score1>=50 and score2>=50:
-    print("You passed!")
-else:
-    print("You need to retake some exams.")
