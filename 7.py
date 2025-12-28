9d6468311e642c5225e45a15c51c29a053453132
Harshini Senthilkumar <harshinisenthil@myyahoo.com>
2025-12-28 23:43:31 +0530

    Delete 7.py

--- 7.py
+++
@@ -1,7 +0,0 @@
-number=int(input("Enter a number:"))
-if (number>0):
-    print("The number you have entered is positive,")
-elif (number<0):
-    print("The number you have entered is negative.")
-else:
-    print ("The number is zero.")
